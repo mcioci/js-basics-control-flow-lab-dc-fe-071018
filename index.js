0f776fb00d0aa5d8c7ab226004b2c52f79fbfe31
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-08-10 00:43:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your code in this file!
+function scuberGreetingForFeet (distance) {
+  console.log("== scuberGreetingForFeet ==");
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  if ((distance > 2000) && (distance < 2500)) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (distance > 2500) {
+    return "No can do.";
+  }
+}
+
+let response = scuberGreetingForFeet (2001);
+
+function ternaryCheckCity (city) {
+  console.log("== ternaryCheckCity ==");
+  let destination = (city === "NYC")
+  ? "Ok, sounds good."
+  : "No go."
+  return destination;
+  }
+
+ternaryCheckCity ("NYC");
+
+function switchOnCharmFromTip (tip) {
+  console.log("== switchOnCharmFromTip ==");
+
+switch (tip) {
+  case "generous":
+    return "Thank you so much."
+    break;
+  case "not as generous":
+    return "Thank you."
+    break;
+  default:
+    return "Bye."
+}
+
+}
+
+switchOnCharmFromTip("generous")
