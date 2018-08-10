edb258bf7b2a63a476b61f92c36276576408c6c0
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-08-10 00:13:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet (distance) {
     return "This one is on me!";
   }
   if ((distance > 2000) && (distance < 2500)) {
-    return "I will gladly take your thirty bucks."
+    return "I will gladly take your thirty bucks.";
   }
   if (distance > 2500) {
     return "No can do"
