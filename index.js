0dc467728ccbfee70a19d9fb1cfd54cb099daf3d
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-08-10 00:02:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet (distance){
-  console.log("== scuberGreetingForFeet ==")
+function scuberGreetingForFeet (distance) {
+  console.log("== scuberGreetingForFeet ==");
   if (distance <= 400) {
     //we haven't defined what distance is at this point!!
     return = 'This one is on me!';
