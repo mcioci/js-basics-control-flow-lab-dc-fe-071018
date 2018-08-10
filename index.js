f55435f00e105e02034f6835e22658f03436a491
meghan.cioci@wework.com <meghan.cioci@wework.com>
2018-08-10 00:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function scuberGreetingForFeet (distance) {
   if (distance > 2000) {
     return "I will gladly take your thirty bucks."
   }
+  if (distance > 2500) {
+    return "No can do"
+  }
 }
 
 let response = scuberGreetingForFeet (199);
